d4527b0d1e8de3846ef687af645946206004195f
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-16 19:41:01 +0700

    Ubuntu

--- test/test_firefox.py
+++ test/test_firefox.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_my_pets(driver_friends):
-    driver_friends.implicitly_wait(10)
-    driver_friends.find_element_by_xpath('//body/nav/div[1]/ul/li[1]/a').click()
-    driver_friends.implicitly_wait(10)
-    assert driver_friends.find_element_by_xpath('//body/div[1]/div/div[1]/h2').text == "AnnaCherdan"
-    driver_friends.close()
+def test_my_pets(driver_friends_firefox):
+    driver_friends_firefox.implicitly_wait(10)
+    driver_friends_firefox.find_element_by_xpath('//body/nav/div[1]/ul/li[1]/a').click()
+    driver_friends_firefox.implicitly_wait(10)
+    assert driver_friends_firefox.find_element_by_xpath('//body/div[1]/div/div[1]/h2').text == "AnnaCherdan"
+    driver_friends_firefox.close()
