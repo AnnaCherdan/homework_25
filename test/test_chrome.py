828d5a6910d1a374f295c8ec33056a40c759d879
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-17 17:30:39 +0700

    win8

--- test/test_chrome.py
+++ test/test_chrome.py
@@ -7,20 +7,45 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_my_pets_info(driver_friends_chrome):
     driver_friends_chrome.implicitly_wait(10)
     driver_friends_chrome.find_element_by_xpath('//body/nav/div[1]/ul/li[1]/a').click()
-    driver_friends_chrome.implicitly_wait(10)
     assert driver_friends_chrome.find_element_by_xpath('//body/div[1]/div/div[1]/h2').text == "AnnaCherdan"
-    driver_friends_chrome.implicitly_wait(10)
+
     pets = 'Питомцев: 5'
     pets_text = driver_friends_chrome.find_element_by_xpath('//body/div[1]/div[1]/div[1]')
     assert pets in pets_text.text
+
     string = driver_friends_chrome.find_elements_by_xpath('//table[@class ="table table-hover"]/tbody/tr')
+
     images = driver_friends_chrome.find_elements_by_css_selector('div#all_my_pets>table>tbody>tr>th>img')
     names = driver_friends_chrome.find_elements_by_xpath("//tr/td[1]")
     animal_type = driver_friends_chrome.find_elements_by_xpath('//tr/td[2]')
     age = driver_friends_chrome.find_elements_by_xpath('//tr/td[3]')
-    driver_friends_chrome.implicitly_wait(10)
-    assert names and animal_type and age != ''
-    assert 'src' in images.text
+    table_pet = driver_friends_chrome.find_elements_by_xpath('//*[@id="all_my_pets"]/table')
+    for i in range(len(string)):
+        assert names[i].text and animal_type[i].text and age[i].text != ''
+        # assert images[i].get_attribute('src') != '', 'WARNING, no IMG'
+        count_img = img
+        if images[i].get_attribute('src') != '':
+            img = len(images)
+        count_img = img
+        count_nam = len(names)
+        assert img == count_nam
+
+
+
+
+
+
+
+
+    # for string in range(len(table_pet)):
+    #     assert string == images
+
+
+
+
+    # table_pet = driver_friends_chrome.find_elements_by_xpath('//*[@id="all_my_pets"]/table')
+    # for i in range(len(table_pet)):
+    #
     # for i in names:
     #     driver_friends_chrome.implicitly_wait(10)
     #     assert images[i].get_attribute('src') != ''
